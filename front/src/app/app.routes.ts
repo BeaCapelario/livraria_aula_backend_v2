02888dc34466cba722ccc6eba644850f3c0e17f6
Lindomar <lindomarbatistao@gmail.com>
2025-09-19 11:32:36 -0300

    X

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
+// import { HomeComponent } from './pages/home/home.component';
+// import { LoginComponent } from './pages/login/login.component';
 import { AutoresPage } from './pages/autores/autores.page';
 import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
+//   { path: '', component: HomeComponent, title: 'Início' },
+//   { path: 'login', component: LoginComponent, title: 'Login' },
+
   // Exemplo de rota protegida:
   { path: 'autores', component: AutoresPage, title: 'Autores', canActivate: [authGuard] },
 
